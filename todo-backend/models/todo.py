05ebd8e343a9709f14492e8f7eb3007798cf7ed5
Owain Williams <owain@andersdigital.com>
2023-01-17 14:33:29 +0000

    create todo model

---
+++ todo-backend/models/todo.py
@@ -0,0 +1,12 @@
+from database import db
+import datetime
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    
+    id = db.Column(db.string(36), primary_key=True, unique=True)
+    name = db.Column(db.string(255), primary_key=True, nullable=False)
+    description = db.Column(db.string(255), nullable=True)
+    deadline = db.Column(db.DateTime, nullable=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
